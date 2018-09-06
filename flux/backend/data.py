f728518f24fa1b3020caec5f12716b3bd351ff07
DavidMChan <david@iridescent.io>
2018-09-06 10:29:21 -0700

    Added MNIST and FashionMNIST datasets

--- flux/backend/data.py
+++ flux/backend/data.py
@@ -9,6 +9,7 @@ from flux.backend.globals import DATA_STORE
 from flux.util.download import maybe_download
 from flux.util.system import untar, unzip, mkdir_p
 
+
 def maybe_download_and_store_zip(url: str, root_key: str, description: str=None) -> str:
     old_keys = []
     if DATA_STORE.is_valid(root_key) and validate_subkeys(root_key, old_keys):
@@ -19,11 +20,14 @@ def maybe_download_and_store_zip(url: str, root_key: str, description: str=None)
     keys = register_to_datastore(data_path, root_key, description)
     return [os.path.join(root_key, k) for k in keys]
 
-def maybe_download_and_store_single_file(url: str, key: str, description: str=None) -> str:
+def maybe_download_and_store_single_file(url: str, key: str, description: str=None, postprocess=None) -> str:
     if not DATA_STORE.is_valid(key):
         # This is where the hard work happens
         # First, we have to download the file into the working directory
-        data_path = maybe_download(url.split('/')[-1], url, DATA_STORE.working_directory)
+        if postprocess is None:
+            data_path = maybe_download(url.split('/')[-1], url, DATA_STORE.working_directory)
+        else:
+            data_path = maybe_download(url.split('/')[-1], url, DATA_STORE.working_directory, postprocess=postprocess)
         DATA_STORE.add_file(key, data_path, description, force=True)
     return key
 
