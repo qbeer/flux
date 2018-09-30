e6a3bb2e4dfbef85a36540931ccb3754d317ba47
LutetiumX <karen.lutetium@gmail.com>
2018-09-29 20:22:02 -0700

    functional celebA

--- samples/celeba_test.py
+++ samples/celeba_test.py
@@ -1,7 +1,5 @@
 from flux.datasets.vision.celeba import CelebA
-from flux.backend.data import maybe_download_and_store_google_drive
+from flux.backend.globals import DATA_STORE
+from flux.backend.data import validate_subkeys
 
-file_pair2 = {"img_align_celeba.zip":"0B7EVK8r0v71pZjFTYXZWM3FlRnM",
-                     "list_attr_celeba.txt":"0B7EVK8r0v71pblRyaVFSWGxPY0U"}
-
-maybe_download_and_store_google_drive(file_pair2, "", "")
+celeba = CelebA()
