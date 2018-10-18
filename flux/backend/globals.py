b37619d8cb95fdd522ea214ce557e75f1e0c3a8f
LutetiumX <karen.lutetium@gmail.com>
2018-10-18 16:47:13 -0700

    backup json doesn't break

--- flux/backend/globals.py
+++ flux/backend/globals.py
@@ -27,29 +27,30 @@ except NameError:
     CURR_CONFIG = get_var('FLUX_CONFIG', 'flux_config.json')
     CONFIG_FILE = CURR_CONFIG
 
+    def retrack_config(reset=False):
+        global CURR_CONFIG, DATA_STORE, CONFIG_FILE
+        all_configs = os.listdir(CONFIG_FOLDER)
+        all_configs.sort(key=lambda x: datetime.strptime(x.split(".")[0], '%Y-%m-%d'), reversed=True)
+        if reset:
+            CURR_CONFIG = get_var('FLUX_CONFIG', 'flux_config.json')
+            CONFIG_FILE = CURR_CONFIG
+        else:
+            index = all_configs.index(CURR_CONFIG) + 1
+            if index < len(all_configs):
+                CURR_CONFIG = all_configs[index]
+            else:
+                log_message("Reached the earliest config file.")
+            CONFIG_FILE = os.path.join(CONFIG_FOLDER, CURR_CONFIG)
+
+        DATA_STORE = DataStore(root_filepath=ROOT_FPATH, config_file=CONFIG_FILE)
+
+    def save_config():
+        # Copy CONFIG_FILE into Current Date file
+        current_date = datetime.now().strftime("%Y-%m-%d") + ".json"
+        shutil.copyfile(CONFIG_FILE, os.path.join(CONFIG_FOLDER, current_date))
+
     RESET_CONFIG = lambda : retrack_config(True)
     RETRACK_CONFIG = retrack_config
     SAVE_CONFIG = save_config
     DATA_STORE = DataStore(root_filepath=ROOT_FPATH, config_file=CONFIG_FILE)
     
-def retrack_config(reset=False):
-    nonlocal CURR_CONFIG, DATA_STORE, CONFIG_FILE
-    all_configs = os.listdir(CONFIG_FOLDER)
-    all_configs.sort(key=lambda x: datetime.strptime(x.split(".")[0], '%Y-%m-%d'), reversed=True)
-    if reset:
-        CURR_CONFIG = get_var('FLUX_CONFIG', 'flux_config.json')
-        CONFIG_FILE = CURR_CONFIG
-    else:
-        index = all_configs.index(CURR_CONFIG) + 1
-        if index < len(all_configs):
-            CURR_CONFIG = all_configs[index]
-        else:
-            log_message("Reached the earliest config file.")
-        CONFIG_FILE = os.path.join(CONFIG_FOLDER, CURR_CONFIG)
-
-    DATA_STORE = DataStore(root_filepath=ROOT_FPATH, config_file=CONFIG_FILE)
-
-def save_config():
-    # Copy CONFIG_FILE into Current Date file
-    current_date = datetime.now().strftime("%Y-%m-%d") + ".json"
-    shutil.copyfile(CONFIG_FILE, os.path.join(CONFIG_FOLDER, current_date))
